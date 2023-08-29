0e03fda1a5ad7f588e6212fbcb6dafb4cdf4d371
NuelUzoma <emcool17@gmail.com>
2023-08-29 02:33:01 +0100

    Test suite

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,5 +6,14 @@ and is a caching system:"""
 BaseCaching = __import__('base_caching')
 
 
-class BasicCache from BaseCaching:
+class BasicCache(BaseCaching):
     """The class that inherits from BaseCaching"""
+    def put(self, key, item):
+        """Adds an item in the cache."""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key."""
+        return self.cache_data.get(key, None)
